35154b0423d83e9d63fc4cccbfc37580f3d87c81
Lucas Ferreira <lucas.htferreira@outlook.com>
2024-05-01 19:24:20 -0300

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,202 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "ferrix"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.UTF-8";
+    LC_IDENTIFICATION = "pt_BR.UTF-8";
+    LC_MEASUREMENT = "pt_BR.UTF-8";
+    LC_MONETARY = "pt_BR.UTF-8";
+    LC_NAME = "pt_BR.UTF-8";
+    LC_NUMERIC = "pt_BR.UTF-8";
+    LC_PAPER = "pt_BR.UTF-8";
+    LC_TELEPHONE = "pt_BR.UTF-8";
+    LC_TIME = "pt_BR.UTF-8";
+  };
+
+  ### Video Drivers ###
+  
+  # OpenGL
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  # Video driver
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  # Nvidia config
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  # ZSH
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      plugins = [ "git" ];
+      theme = "agnoster";
+    };
+  };
+  users.defaultUserShell = pkgs.zsh;
+
+  # Pipewire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+};
+
+  # Configure keymap in X11
+  services.xserver = {
+    enable = true;
+    xkb.layout = "br";
+    xkb.variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "br-abnt2";
+
+  # Hyprland
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  # Display Manager
+  services.displayManager = {
+    sddm.enable = true;
+    sddm.wayland.enable = true;
+    sddm.autoNumlock = true;
+    defaultSession = "hyprland";
+  };
+
+  # Udisks2
+  services.udisks2.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.lucas = {
+    isNormalUser = true;
+    description = "Lucas Ferreira";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  # Fonts
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    cascadia-code
+    nerdfonts
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  libsForQt5.sddm
+  libsForQt5.qt5ct
+  libsForQt5.dolphin
+  libsForQt5.polkit-kde-agent
+  libsForQt5.qt5.qtwayland
+  nwg-look
+  vim
+  wget
+  git
+  hyprland
+  swww
+  dunst
+  xdg-desktop-portal-hyprland
+  xdg-desktop-portal-gnome
+  udiskie
+  udisks
+  libnvme
+  waybar
+  rofi-wayland-unwrapped
+  pamixer
+  playerctl
+  alacritty
+  fontconfig
+  firefox-bin
+  vscode
+  spotify
+  ];
+
+  # Session variables
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    QT_QPA_PLATFORM = lib.mkDefault "wayland";
+    QT_QPA_PLATFORMTHEME = lib.mkDefault "qt5ct";
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+}
---
+++ home.nix
@@ -0,0 +1,119 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "lucas";
+  home.homeDirectory = "/home/lucas";
+
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # Home Packages
+  home.packages = with pkgs; [
+    feh # Terminal Image Viewer
+    fastfetch # Fetch
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    ".icons/default".source = "${pkgs.bibata-cursors}/share/icons/Bibata-Modern-Ice"; # Icon theme
+    ".config/waybar".source = ./home/external/waybar;
+  };
+
+  # Git
+  programs.git = {
+    enable = true;
+    userName = "Lucas Ferreira";
+    userEmail = "lucas.htferreira@outlook.com";
+  };
+
+  # XDG
+  xdg = {
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "image/*" = [ "feh.desktop" ];
+      };
+    };
+
+    desktopEntries = {
+      feh = {
+        name = "Feh";
+        type = "Application";
+        genericName = "Image Viewer";
+        exec = "feh -.";
+        terminal = false;
+        categories = [ "Graphics" "Viewer" ];
+        mimeType = [ "image/jpeg" "image/png" "image/gif" "image/svg+xml" "image/webp" "image/bmp" ];
+      };
+    };
+  };
+
+  # GTK
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.adw-gtk3;
+      name = "adw-gtk3-dark";
+    };
+    cursorTheme.name = "Bibata-Modern-Ice";
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus Icon Theme";
+    };
+  };
+
+  # Qt
+  qt = {
+    enable = true;
+    platformTheme.name = "qtct";
+    style.name = "breeze-dark";
+    style.package = pkgs.libsForQt5.breeze-qt5;
+  };
+
+  # Alacritty
+  programs.alacritty = {
+    enable = true;
+    settings = {
+	    window = {
+        opacity = 0.8;
+        blur = true;
+	      dimensions = {
+		      columns = 140;
+		      lines = 36;
+	      };
+	    };
+	    font = {
+	      size = 12.5;
+	      normal = {
+	        family = "CaskaydiaCove Nerd Font";
+		      style = "Regular";
+	      };
+	    };
+    };
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/lucas/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
